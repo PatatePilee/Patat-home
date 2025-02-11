434cf5edb9aa5474cb3eb0dc44e0ba3b122382a7
Your Namecomposer <antoine.ducolomb@etu.univ-lyon1.fr>
2025-02-11 14:16:28 +0100

    google anal

--- patatehome/app/layout.tsx
+++ patatehome/app/layout.tsx
@@ -16,6 +16,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" className={font.className}>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5ZVSNRSP');
+            `,
+          }}
+        />
+      </head>
       <body
         suppressHydrationWarning
         className="bg-black min-h-screen text-white"
@@ -26,7 +39,7 @@ export default function RootLayout({
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
+          />
         </noscript>
         <Providers>
           <Header />
